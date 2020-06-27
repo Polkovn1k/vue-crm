35b6138b6fcb9908041b418337a02f9f1b9c08d2
Polkovn1k <wa0s@narod.ru>
2020-06-27 22:59:42 +0300

    lesson-2: added layout & view components. Described routes

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,29 @@
 <template>
   <div id="app">
-    <router-view/>
+    <component :is="layout">
+      <!--<router-view/>-->
+    </component>
   </div>
 </template>
 
+<script>
+import MainLayout from './layouts/MainLayout.vue'
+import ModalLayout from './layouts/ModalLayout'
+
+export default {
+  components: {
+    MainLayout,
+    ModalLayout
+  },
+  computed: {
+    layout () {
+      return (this.$route.meta.layout || 'modal-layout')
+    }
+  }
+}
+</script>
+
 <style lang="scss">
+  @import '~materialize-css/dist/css/materialize.min.css';
+  @import 'assests/index.css';
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,56 @@ const routes = [
   {
     path: '/',
     name: 'Home',
+    meta: { layout: 'main-layout' },
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    meta: { layout: 'modal-layout' }, // С помощью мета мы выясняем какой layout будет подгружаться
+    component: () => import('../views/Login')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    meta: { layout: 'modal-layout' },
+    component: () => import('../views/Register')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/detail-record',
+    name: 'detail-record',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/DetailRecord.vue')
+  },
+  {
+    path: '/history',
+    name: 'history',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'record',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Record.vue')
   }
 ]
 
